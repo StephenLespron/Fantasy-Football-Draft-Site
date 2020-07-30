18caf61d2a176e8b40918218df37cc60215a8172
StephenLespron <stephen.lespron@gmail.com>
2020-07-30 16:58:23 -0700

    a

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ app.get(`/api/getTeams/:draftId`, dataCtrl.getTeams);
 app.post('/sendEmail', emailCtrl);
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	res.sendFile(path.join(__dirname + '../build/index.html'));
 });
 
 massive({
